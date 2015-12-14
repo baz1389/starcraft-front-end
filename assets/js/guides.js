9ad2084a2bfcfa918cea1a402afd23304e52ce0e
Ben <benjamin.a.spencer@gmail.com>
2015-12-14 13:09:34 -0500

    templates the create jquery function.

--- assets/js/guides.js
+++ assets/js/guides.js
@@ -3,7 +3,15 @@
 $(document).ready(function() {
 
 
+  $('#createNewGuide').on('submit', function(e) {
+    e.preventDefault();
 
+    var data = var data = form2object(this);
+    console.log('the form will send ' + JSON.stringify(data, null, 4));
+
+    guide_api.createGuide(data, createCb)
+
+  });
 
 
 
