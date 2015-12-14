c7477163201dcde0c73fe7ee46fae83d610a6bb4
Ben <benjamin.a.spencer@gmail.com>
2015-12-14 13:53:05 -0500

    templating create functions

--- assets/js/guides.js
+++ assets/js/guides.js
@@ -6,11 +6,32 @@ $(document).ready(function() {
   $('#createNewGuide').on('submit', function(e) {
     e.preventDefault();
 
-    var data = var data = form2object(this);
+    var data = form2object(this);
+
+
     console.log('the form will send ' + JSON.stringify(data, null, 4));
 
-    guide_api.createGuide(data, createCb)
+    guide_api.createGuide(data, function(event) {
 
+    })
+
+  });
+
+
+    $("#create-blog").on("submit", function(event){
+    event.preventDefault();
+    var credentials = formDataToObject(this);
+    blogRequest.create(credentials, function(error, data){
+    blogRequest.getAll(function(error, data){
+      $("#showAllBlogTableBody").empty();
+      $("#display-blogs-table").show();
+      $("#one-blog").hide();
+      var template = Handlebars.compile($("#showAllBlogHandlebar").html());
+      $('#result').val(JSON.stringify(data, null, 4)); //logs to test box
+      var newHTML = template({blogs: data.blogs});
+      $("#showAllBlogTableBody").html(newHTML);
+      });
+    });
   });
 
 
@@ -20,7 +41,4 @@ $(document).ready(function() {
 
 
 
-
-
-
-}
+});
