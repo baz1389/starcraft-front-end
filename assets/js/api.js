fb7b34412c77fdc894dbbf93c373dbc133566b0f
Ben <benjamin.a.spencer@gmail.com>
2015-12-17 15:36:56 -0500

    adds heroku URL to api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var guide_api = {
-  url: 'http://localhost:3000', //heroku url here with https and .com (no /)
+  url: 'https://sheltered-refuge-9436.herokuapp.com', //http://localhost:3000
 
   ajax: function(config, cb) {
     $.ajax(config).done(function(data, textStatus, jqxhr) {
